133001a6db3fb3811254e32c5a8e1fffe449cb2e
cord <cord@localhost>
2011-11-16 23:17:31 +0100

    read Graupner HoTT SUM signal

--- README.txt
+++ README.txt
@@ -1,1 +1,11 @@
 Cool project for R/C, telemetry and engine control
+
+History
+
+14.11.2011
+	- use timer1 to capture pulsewidth and print via semihosting
+	
+16.11.2011
+	- read Graupner HoTT SUM signal, connect SUM signal to P1[18] and P1[19]
+	- generate trigger signal for oscilloscope at P1[20]
+	- generate threshold signal for channel 1 to show some action by switching the LED (P0[22])
--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-// main.c Uses timer1 to capture pulsewidth and print via semihosting
+// main.c Read Graupner HoTT SUM signal
 // Copyright (C) 2011  Cord Johannmeyer
 //
 // This program is free software; you can redistribute it and/or
@@ -15,6 +15,13 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+//
+// Purpose
+/**
+	- read Graupner HoTT SUM signal, connect SUM signal to P1[18] and P1[19]
+	- generate trigger signal for oscilloscope at P1[20]
+	- generate threshold signal for channel 1 to show some action
+*/
 
 #ifdef __USE_CMSIS
 #include "LPC17xx.h"
@@ -33,12 +40,16 @@ __CRP const unsigned int CRP_WORD = CRP_NO_CRP ;
 #include "lpc17xx_gpio.h"
 #include "lpc17xx_timer.h"
 
+// other definitions and declarations
+#define LED (1<<22)
+#define TRIG (1<<20)
 
-// TODO: insert other definitions and declarations here
 uint32_t volatile Timer1CaptureValue = 0;
-uint32_t volatile Timer1CaptureLowTime = 0;
-uint32_t volatile Timer1CaptureHighTime = 0;
-uint8_t CaptureOnRisingEdge = ENABLE;
+uint32_t volatile Timer1CaptureLowTime[32];		/// not used, for information only
+uint32_t volatile Timer1CaptureHighTime[32];	/// receiver channel 1 to n
+uint32_t volatile lowIndex = 0;
+uint32_t volatile highIndex = 0;
+uint32_t volatile max = 0;
 
 /************************** PRIVATE FUNCTIONS *************************/
 void setTimer1Capture(void);
@@ -57,39 +68,46 @@ void TIMER1_IRQHandler(void)
 	{
 		TIM_ClearIntCapturePending(LPC_TIM1,0);
 		uint32_t capt = TIM_GetCaptureValue(LPC_TIM1,0);
-		Timer1CaptureLowTime = capt - Timer1CaptureValue;
+		Timer1CaptureLowTime[lowIndex] = capt - Timer1CaptureValue;
 		Timer1CaptureValue = capt;
+		lowIndex++;
+		if(Timer1CaptureLowTime[lowIndex] > 5000)
+		{
+			lowIndex = 0;
+			highIndex = 0; }
+		else
+		{
+			lowIndex++;
+		}
 	}
 	else if (TIM_GetIntCaptureStatus(LPC_TIM1,1)){
 		TIM_ClearIntCapturePending(LPC_TIM1,1);
 		uint32_t capt = TIM_GetCaptureValue(LPC_TIM1,1);
-		Timer1CaptureHighTime = capt - Timer1CaptureValue;
+		Timer1CaptureHighTime[highIndex] = capt - Timer1CaptureValue;
 		Timer1CaptureValue = capt;
+		if(Timer1CaptureHighTime[highIndex] > 5000)
+		{
+			max = highIndex;
+			lowIndex = 0;
+			highIndex = 0; }
+		else
+		{
+			highIndex++;
+		}
 	}
+	if(lowIndex == 0) GPIO_SetValue(1, TRIG);
+	else GPIO_ClearValue(1, TRIG);
 }
 
+/*********************************************************************//**
+ * @brief		set up TIMER1 capture register
+ * @param[in]	None
+ * @return 		None
+ **********************************************************************/
 void setTimer1Capture(void)
 {
     TIM_CAPTURECFG_Type TIM_CaptureConfigStruct;
-//    TIM_CAPTURECFG_Type TIM_CaptureConfigStruct1;
 
-    TIM_CaptureConfigStruct.CaptureChannel = 0; // use channel 0, CAPn.0
-    TIM_CaptureConfigStruct.RisingEdge = ENABLE; // Enable capture on CAPn.0 rising edge
-    TIM_CaptureConfigStruct.FallingEdge = DISABLE; // Disable capture on CAPn.0 falling edge
-    TIM_CaptureConfigStruct.IntOnCaption = ENABLE; // Generate capture interrupt
-    TIM_ConfigCapture(LPC_TIM1, &TIM_CaptureConfigStruct);
-
-    TIM_CaptureConfigStruct.CaptureChannel = 1; // use channel 1, CAPn.0
-    TIM_CaptureConfigStruct.RisingEdge = DISABLE; // Disable capture on CAPn.0 rising edge
-    TIM_CaptureConfigStruct.FallingEdge = ENABLE; // Enable capture on CAPn.0 falling edge
-    TIM_CaptureConfigStruct.IntOnCaption = ENABLE; // Generate capture interrupt
-    TIM_ConfigCapture(LPC_TIM1, &TIM_CaptureConfigStruct);
-
-    TIM_ResetCounter(LPC_TIM1);
-}
-
-int main(void) {
-	
 	//Config P1.18 as CAP1.0 | LPC1769 (LQFP100) Pin 32 | LPCXpresso PAD1 (not linked to base board)
 	PINSEL_CFG_Type PinCfg;
 	PinCfg.Funcnum = 3;
@@ -108,21 +126,47 @@ int main(void) {
 	TIM_ConfigStruct.PrescaleValue	= 1;
 	TIM_Init(LPC_TIM1, TIM_TIMER_MODE, &TIM_ConfigStruct);
 
+    TIM_CaptureConfigStruct.CaptureChannel = 0; // use channel 0, CAPn.0
+    TIM_CaptureConfigStruct.RisingEdge = ENABLE; // Enable capture on CAPn.0 rising edge
+    TIM_CaptureConfigStruct.FallingEdge = DISABLE; // Disable capture on CAPn.0 falling edge
+    TIM_CaptureConfigStruct.IntOnCaption = ENABLE; // Generate capture interrupt
+    TIM_ConfigCapture(LPC_TIM1, &TIM_CaptureConfigStruct);
+
+    TIM_CaptureConfigStruct.CaptureChannel = 1; // use channel 1, CAPn.0
+    TIM_CaptureConfigStruct.RisingEdge = DISABLE; // Disable capture on CAPn.0 rising edge
+    TIM_CaptureConfigStruct.FallingEdge = ENABLE; // Enable capture on CAPn.0 falling edge
+    TIM_CaptureConfigStruct.IntOnCaption = ENABLE; // Generate capture interrupt
+    TIM_ConfigCapture(LPC_TIM1, &TIM_CaptureConfigStruct);
+
+    TIM_ResetCounter(LPC_TIM1);
+}
+
+/*********************************************************************//**
+ * @brief		the main program
+ **********************************************************************/
+int main(void) {
+	
+	GPIO_SetDir(0, LED, 1);			// LEDs on PORT0 defined as Output
+	GPIO_SetDir(1, TRIG, 1);		// Trigger signal for oscilloscope
+
     setTimer1Capture();
 
     NVIC_SetPriority(TIMER1_IRQn, ((0x01 << 3) | 0x01)); // preemption = 1, sub-priority = 1
     NVIC_EnableIRQ(TIMER1_IRQn); // Enable interrupt for timer 1
     TIM_Cmd(LPC_TIM1,ENABLE);
 
-    uint32_t T1CntVal = LPC_TIM1->TC;
-    Timer0_us_Wait(10000);
-    uint32_t T1CntVal1 = LPC_TIM1->TC;
-    printf("T1CntVal = %d T1CntVal = %d diff = %d \n", T1CntVal, T1CntVal1, T1CntVal1 - T1CntVal);
-    // Enter an infinite loop, just incrementing a counter
+    // Enter an infinite loop
     static volatile int i = 0 ;
 	while(1) {
-		printf("LowTime = %d  HighTime = %d\n", Timer1CaptureLowTime, Timer1CaptureHighTime);
-		Timer0_Wait(1000);
+		if(Timer1CaptureHighTime[0] > 1000) GPIO_SetValue(0, LED);
+		else GPIO_ClearValue(0, LED);
+//		int j;
+//		for(j=0; j<=max; j++) {
+//			printf("ch%d = %d ", j+1, Timer1CaptureHighTime[j]);
+//		}
+//		printf("\n");
+//		printf("LowTime = %d  HighTime = %d\n", Timer1CaptureLowTime[0], Timer1CaptureHighTime[0]);
+//		Timer0_Wait(1000);
 		i++ ;
 	}
 	return 0 ;
